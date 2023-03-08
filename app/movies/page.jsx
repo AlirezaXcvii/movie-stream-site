2bd449add69aa17ccd562d246cf40ee77b3569f2
Arx <alireza.xcvii@gmail.com>
2023-03-08 21:18:24 +0330

    movies page added

--- app/movies/page.jsx
+++ app/movies/page.jsx
@@ -1,25 +1,70 @@
-'use client'
+'use client';
 import Card from '@/components/Card';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react';
 
+async function fetchMovies(page){
+  const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=b9e4f85c3b6987a76b17bb7579bc1ac8&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}`);
+  const data = await response.json();
+  return data
+}
 
-
-export default function Movies() {
-
-  const [movies , SetMovies] = useState([])
+export default function MoviePage() {
+  const [movies, setMovies] = useState([]);
+  const [page, setPage] = useState(3); // start at page 3
+  const loadMoreButtonRef = useRef(null);
 
   useEffect(() => {
-    for(let i=1; i<=5 ; i++){
-      fetch(`https://api.themoviedb.org/3/discover/movie?api_key=b9e4f85c3b6987a76b17bb7579bc1ac8&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${i}`)
-      .then((response) => response.json())
-      .then((data) => SetMovies(prev => [...prev, data.results.map(movie => movie)]))
-  }}, [])
+    async function getData() {
+      // Only fetch data for the first three pages
+      for (let i = 1; i <= 3; i++) {
+        const data = await fetchMovies(i);
 
+        setMovies(prevMovies => {
+          const uniqueMovies = data.results.filter(movie => !prevMovies.some(prevMovie => prevMovie.id === movie.id));
+          return [...prevMovies, ...uniqueMovies];
+        });
+      }
+    }
+
+    getData();
+  }, []);
+
+  useEffect(() => {
+    async function getData() {
+      const data = await fetchMovies(page);
+
+      setMovies(prevMovies => {
+        const uniqueMovies = data.results.filter(movie => !prevMovies.some(prevMovie => prevMovie.id === movie.id));
+        return [...prevMovies, ...uniqueMovies];
+      });
+    }
+
+    getData();
+  }, [page]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.innerHeight + window.scrollY >= document.body.scrollHeight - 100) {
+        setPage(prevPage => prevPage + 1);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+  
   return (
     <div className='max-w-[1400px] flex flex-wrap mx-auto'>
-        {movies.length >= 0 &&  movies.map(item => item.map(movie => (
-           <Card data={movie} key={movie.id} />
-          )))}
+      {/* Render the movie results */}
+      
+      {movies.map(movie => (
+        <Card data={movie} key={movie.id} />
+      ))}
+
+      {/* Render a dummy element to reference for infinite scrolling */}
+      <div ref={loadMoreButtonRef} />
+
     </div>
-  )
+  );
 }
